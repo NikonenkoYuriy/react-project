d17164b012a1697f4f80cfabceb20c9cf67d004c
NikonenkoYuriy <Lyudmila_Nikonenko@ukr.net>
2020-09-16 16:20:15 +0300

    Updates

---
+++ precache-manifest.1515b39b5a8c67dd9447b14e813535a4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1ab2419a875e028d9f6aaa7734e3e12",
+    "url": "/react-project/index.html"
+  },
+  {
+    "revision": "2aa4ddb3f02455901f26",
+    "url": "/react-project/static/js/2.0928ce5b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-project/static/js/2.0928ce5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31805f8a985a55cc4afb",
+    "url": "/react-project/static/js/main.7e2bce6a.chunk.js"
+  },
+  {
+    "revision": "d48b6be2e69f2ae7c9ac",
+    "url": "/react-project/static/js/runtime-main.5e88d720.js"
+  }
+]);
